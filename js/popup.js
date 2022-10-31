8da1bcf9d03ca24e8b475ea4d3b14cc0ab2dc8d3
ifhito <hito01010101@gmail.com>
2022-11-01 00:18:15 +0900

    fixed: コードがわかりやすいように変更する

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,19 @@
 //call when onclick of #sendSlack
 document.getElementById('sendSlack').onclick = () => {
+    //get comment to document
+    const comment = document.getElementById('comment').value;
+    //send sendSlack-function and comment
+    getCurrentTabUrlandTitle(comment)
+};
+
+//function of get current open tab url and title
+const getCurrentTabUrlandTitle = (comment) => {
+    //setting of getting infomation by current tab
+    const queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+
     //function that send message to slack
     const sendSlack = async (message) => {
         //read webhookUrl from localStorage
@@ -15,18 +29,7 @@ document.getElementById('sendSlack').onclick = () => {
             console.log(response.status);
         });
     };
-    //get comment to document
-    const comment = document.getElementById('comment').value;
-    //send sendSlack-function and comment
-    getCurrentTabUrlandTitle(sendSlack,comment)
-};
-//function of get current open tab url and title
-const getCurrentTabUrlandTitle = (sendSlack,comment) => {
-    //setting of getting infomation by current tab
-    const queryInfo = {
-        active: true,
-        currentWindow: true
-    };
+
     //get tab info
     chrome.tabs.query(queryInfo, async (tab) => {
         const Url = String(tab[0].url);
